aa3d023d59f02d57574db57c88f21771594039e2
MarquisSewell <MarquisSewell@users.noreply.github.com>
2014-01-25 12:26:39 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+
+var red = [0, 100, 63];
+var black = [0, 100, 6];
+
+
+var myName = "Yung Sewell"
+letterColors = [red, black];
+if(9 > 4) {
+    bubbleShape ="circle";
+}
+
+else {
+    bubbleShape ="square";
+}
+
+
+
+drawName(myName, letterColors);
+bounceBubbles()
