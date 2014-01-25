c417e5c8c5e8fba7b8117c926510962c31e0649a
MarquisSewell <MarquisSewell@users.noreply.github.com>
2014-01-25 12:35:09 -0600

    Create main2.js

---
+++ main2.js
@@ -0,0 +1,18 @@
+var red = [0, 100, 63];
+var black = [0, 100, 6];
+
+
+var myName = "Tebz"
+letterColors = [red, black];
+if(9 > 4) {
+    bubbleShape ="circle";
+}
+
+else {
+    bubbleShape ="square";
+}
+
+
+
+drawName(myName, letterColors);
+bounceBubbles()
