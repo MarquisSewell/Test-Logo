4f0555c7be7ec22c1f3773acb36e81f0c2452938
MarquisSewell <MarquisSewell@users.noreply.github.com>
2014-01-25 12:34:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var red = [0, 100, 63];
 var black = [0, 100, 6];
 
 
-var myName =" YungSewell&Tebz "
+var myName =" YungSewell"
 letterColors = [red, black];
 if(9 > 4) {
     bubbleShape ="circle";
